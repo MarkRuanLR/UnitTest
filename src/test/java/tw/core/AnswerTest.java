737a75f487fd6a135cdf34e9d05c7ae4e81f9783
strawls <mark_ruan@icloud.com>
2018-04-26 11:36:56 +0800

    Finish AnswerTest

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,41 @@
 package tw.core;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.model.Record;
+
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    @Test
+    public void shouid_return_correct_number() {
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        Assert.assertThat(answer.getIndexOfNum("3"), is(2));
+    }
+
+    @Test
+    public void shouid_pass_correct_number() throws OutOfRangeAnswerException {
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        answer.validate();
+    }
+
+    @Test(expected = OutOfRangeAnswerException.class)
+    public void shouid_throw_Error_with_wrong_number() throws OutOfRangeAnswerException {
+        Answer answer = Answer.createAnswer("1 2 2 4");
+        answer.validate();
+    }
+
+    @Test
+    public void shouid_return12_with_input_1243_answer_1234() {
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        Answer input = Answer.createAnswer("1 2 4 3");
+        Record record = answer.check(input);
+        Assert.assertThat(record.getValue(),is(new int[]{2,2}));
+    }
 
 }
--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,26 +1,42 @@
 package tw.core.generator;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import tw.commands.InputCommand;
+import tw.core.Answer;
 import tw.core.Game;
+import tw.core.exception.OutOfRangeAnswerException;
 import tw.views.GameView;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
-    @Mock
-    GameView gameView;
-    InputCommand inputCommand;
-    AnswerGenerator answerGenerator;
-    Game game;
+    @Test
+    public void should_return_right_answer() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
 
+        Answer answer = Answer.createAnswer("1 2 3 4");
+        Assert.assertEquals(answer.toString(), answerGenerator.generate().toString());
+    }
+
+    @Test(expected = OutOfRangeAnswerException.class)
+    public void should_throw_Error() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 10");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        answerGenerator.generate();
     }
 }
 
